d01d67b3b59ad74c95ee94d18dc5cf571389449c
alexey-y-a <lexus2157@mail.ru>
2026-01-22 23:56:47 +0300

    step_006 done

--- services/kv-service/internal/http/handlers.go
+++ services/kv-service/internal/http/handlers.go
@@ -6,15 +6,33 @@ import (
 	"time"
 
 	"github.com/alexey-y-a/go-txlog-microservices/libs/logger"
+	"github.com/alexey-y-a/go-txlog-microservices/services/kv-service/internal/store"
 )
+
+type Handler struct {
+    store *store.Store
+}
+
+func NewHandler(s *store.Store) *Handler {
+    return &Handler {
+        store: s,
+    }
+}
 
 type healthResponse struct {
     Status string `json:"status"`
     Time string   `json:"time"`
 }
 
-func RegisterRoutes(mux *http.ServeMux) {
+func (h *Handler) RegisterRoutes(mux *http.ServeMux) {
     mux.HandleFunc("/health", healthHandler)
+
+    mux.HandleFunc("/kv/set", h.setHandler)
+
+    mux.HandleFunc("kv/get", h.getHandler)
+
+    mux.HandleFunc("kv/del", h.deleteHandler)
+
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
@@ -32,4 +50,134 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
     if err != nil {
         log.Error().Err(err).Msg("failed to write health response")
     }
+}
+
+type SetRequest struct {
+    Key string `json:"key"`
+    Value string `json:"value"`
+}
+
+type commonResponse struct {
+    Status string `json:"status"`
+    Message string `json:"message,omitempty"`
+}
+
+func (h *Handler) setHandler(w http.ResponseWriter, r *http.Request) {
+    log := logger.L().With().Str("handler", "set").Logger()
+
+    if r.Method != http.MethodPost {
+        w.WriteHeader(http.StatusMethodNotAllowed)
+        return
+    }
+
+    var req SetRequest
+    decoder := json.NewDecoder(r.Body)
+    err := decoder.Decode(&req)
+    if err != nil {
+        log.Error().Err(err).Msg("failed to decode set request")
+
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    if req.Key == "" {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    err = h.store.Set(req.Key, req.Value)
+    if err != nil {
+        log.Error().Err(err).Str("key", req.Key).Msg("store set failed")
+
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+
+     response := commonResponse {
+         Status: "ok",
+         Message: "value set",
+     }
+
+     w.Header().Set("Content-Type", "application/json")
+     w.WriteHeader(http.StatusOK)
+
+     err = json.NewEncoder(w).Encode(response)
+     if err != nil {
+         log.Error().Err(err).Msg("failed to write set response")
+     }
+}
+
+type getResponse struct {
+    Status string `json:"status"`
+    Value string `json:"value, omitempty"`
+}
+
+func (h *Handler) getHandler(w http.ResponseWriter, r *http.Request) {
+	log := logger.L().With().Str("handler", "get").Logger()
+
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	key := r.URL.Query().Get("key")
+	if key == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	value, ok := h.store.Get(key)
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	response := getResponse{
+		Status: "ok",
+		Value:  value,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to write get response")
+	}
+}
+
+func (h *Handler) deleteHandler(w http.ResponseWriter, r *http.Request) {
+	log := logger.L().With().Str("handler", "delete").Logger()
+
+	if r.Method != http.MethodDelete {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	key := r.URL.Query().Get("key")
+	if key == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err := h.store.Delete(key)
+	if err != nil {
+		log.Error().Err(err).Str("key", key).Msg("store delete failed")
+
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	response := commonResponse{
+		Status:  "ok",
+		Message: "key deleted",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to write delete response")
+	}
 }
--- services/kv-service/internal/store/store.go
+++ services/kv-service/internal/store/store.go
@@ -1,7 +1,68 @@
 package store
 
-type Store struct{
+import (
+	"fmt"
+	"sync"
 
+	"github.com/alexey-y-a/go-txlog-microservices/libs/txlog"
+)
+
+type Store struct{
+    mu sync.RWMutex
+    data map[string]string
+    log txlog.Log
 }
 
+func NewStore(log txlog.Log) *Store {
+    return &Store {
+        data: make(map[string]string),
+        log: log,
+    }
+}
+
+func (s *Store) Set(key, value string) error {
+    event := txlog.Event {
+        Key: key,
+        Value: value,
+        Op: "set",
+    }
+
+    err := s.log.Append(event)
+    if err != nil {
+        return fmt.Errorf("store: append set event: %w", err)
+    }
+
+    s.mu.Lock()
+    s.data[key] = value
+    s.mu.Unlock()
+
+    return nil
+}
+
+func (s *Store) Get(key string) (string, bool) {
+    s.mu.RLock()
+    value, ok :=  s.data[key]
+    s.mu.RUnlock()
+
+    return value, ok
+}
+
+func (s *Store) Delete(key string) error {
+    event := txlog.Event {
+        Key: key,
+        Value: "",
+        Op: "delete",
+    }
+
+    err := s.log.Append(event)
+    if err != nil {
+        return fmt.Errorf("store: append delete event: %w", err)
+    }
+
+    s.mu.Lock()
+    delete(s.data, key)
+    s.mu.Unlock()
+
+    return nil
+}
 
