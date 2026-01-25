d382c18ae3c8d9a2204cf913b032ebef68a9a59a
alexey-y-a <lexus2157@mail.ru>
2026-01-25 21:21:00 +0300

    step_012 done

--- libs/logger/logger.go
+++ libs/logger/logger.go
@@ -2,23 +2,30 @@ package logger
 
 import (
 	"os"
+	"sync"
+	"time"
 
 	"github.com/rs/zerolog"
 )
 
 var (
     log zerolog.Logger
-    inited bool
+    once sync.Once
 )
 
 func Init() {
-    log = zerolog.New(os.Stdout).With().Timestamp().Logger()
-    inited = true
+	once.Do(func() {
+		zerolog.TimeFieldFormat = time.RFC3339
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+		log = zerolog.New(os.Stdout).
+			With().
+			Timestamp().
+			Logger()
+	})
 }
 
 func L() zerolog.Logger {
-    if !inited {
-        Init()
-    }
+    Init()
     return log
 }
